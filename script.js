b3e13f323d3d64546dd175340c163d0b899a956a
Yuzu <powerpizza688@gmail.com>
2025-05-23 03:41:52 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", function () {
   const facebookButton = document.querySelector(".btn");
   facebookButton.addEventListener("click", function () {
-    window.open("https://www.facebook.com/share/18xWVdhU7X/", "_blank");
+    window.open("https://yuzu7zz.github.io/Code-Python/", "_blank");
   });
 });
